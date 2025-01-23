7f8196d54ad251f63aeab75367d705a011d60971
Ayaka Ito <a.8.32.noko.info@gmail.com>
2025-01-23 12:43:54 -0800

    done

--- tests/counter.test.js
+++ tests/counter.test.js
@@ -4,4 +4,12 @@
 // Both test cases need to pass
 // HINT: Use the jest `resolves` matcher so that it is easier to work with async functions
 
-const counter = require('../src/counter')
+const counter = require("../src/counter");
+
+test("counter(10, 'increment') will eventualy return 10", () => {
+  expect(counter(10, "increment")).resolves.toBe(10);
+});
+
+test("counter(10, 'decrement') will eventualy return 0", () => {
+  expect(counter(10, "decrement")).resolves.toBe(0);
+});
